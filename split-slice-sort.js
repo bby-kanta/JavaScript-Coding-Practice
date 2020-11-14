32699e1c15cc1d4f9b6049d94f351a336f87a150
Kanta <kan.kan19980928@gmail.com>
2020-11-14 16:43:20 +0900

    sortは破壊的メソッド！！

--- split-slice-sort.js
+++ split-slice-sort.js
@@ -34,7 +34,7 @@ kiwi,80
 
 const printSortedData = (items) => {
   const array_data = items.split("\n").slice(1,-1); //split("\n")で配列を作る → sliceはいらないように見えるが、無いとdataの最初と最後に改行があるので''という空の要素が出来てしまう。それを消すために必要。
-  const sort_data  = array_data.slice().sort(function(a,b){  //ここのsliceは本当にいらないはず。無くても同じ結果が帰ってきた。
+  const sort_data  = array_data.slice().sort(function(a,b){  //ここのsliceは本当にいらないはず。無くても同じ結果が帰ってきた。 → sortは破壊的メソッドといって元の配列を壊してしまう。なのでsliceを使いコピーを作成している。
       const a_plice = Number(a.split(",")[1]);
       const b_plice = Number(b.split(",")[1]);
       if (a_plice < b_plice) return -1;
